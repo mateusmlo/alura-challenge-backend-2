866abd415e3ea7a54dfd6ea7f2fbd03892516482
Mateus Oliveira <m.oliveira@envoy.tec.br>
2022-07-01 18:07:40 -0300

    WIP implement swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +15,19 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Finance Control API Docs')
+    .addTag('auth')
+    .addTag('expenses')
+    .addTag('receipts')
+    .addTag('summary')
+    .addTag('users')
+    .setVersion('1.0')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, doc);
 
   await app.listen(3001);
   logger.log(`🔌 Application listening @ localhost:3001`);
